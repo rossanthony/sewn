42c9394f73f80164bceb8e31d1bcafe8a74ac882
rossanthony <ross@velo33.com>
2015-10-31 21:35:30 +0000

    Refactoring the answer to part 1 of the assignment

--- 1_list_anchors.py
+++ 1_list_anchors.py
@@ -1,11 +1,7 @@
 from lxml import html
-import requests
 
-page = requests.get('http://www.dcs.bbk.ac.uk/~martin/sewn/ls3/testpage.html')
-tree = html.fromstring(page.text)
-
-#Get all anchors on the page
-anchors = tree.xpath('//a')
+domTree = html.parse('http://www.dcs.bbk.ac.uk/~martin/sewn/ls3/testpage.html')
+anchors = domTree.xpath('//a') # Returns an array of all anchors on the page
 
 for link in anchors:
     print '<a href="' + link.attrib['href'] + '">' + link.text_content() + '</a>'
